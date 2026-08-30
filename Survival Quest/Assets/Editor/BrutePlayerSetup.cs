5426e4ebb87f1a0c2eb6eed8bdfc2a25beaf4d9b
Mohammadd Fahad Siddiqui <mohdfahadsiddiqui1248@gmail.com>
2026-08-30 23:31:48 +0530

    feat: attach procedural environment to sample scene

--- Survival Quest/Assets/Editor/BrutePlayerSetup.cs
+++ Survival Quest/Assets/Editor/BrutePlayerSetup.cs
@@ -3,6 +3,7 @@ using UnityEngine;
 using UnityEditor;
 using System.Collections.Generic;
 using SurvivalGame;
+using SurvivalQuest.Environment;
 
 /// <summary>
 /// Sets up the Brute character as the Player prefab.
@@ -37,14 +38,12 @@ public static class BrutePlayerSetup
         {
             GameObject root = editScope.prefabContentsRoot;
 
-            // 1. Remove old character-1 child if it exists and is not the Brute
             Transform oldChar = root.transform.Find("character-1");
             if (oldChar != null)
             {
                 Object.DestroyImmediate(oldChar.gameObject);
             }
 
-            // Remove any leftover old Armature or meshes directly under root
             for (int i = root.transform.childCount - 1; i >= 0; i--)
             {
                 Transform child = root.transform.GetChild(i);
@@ -55,18 +54,15 @@ public static class BrutePlayerSetup
                 }
             }
 
-            // 2. Instantiate Brute.fbx as child
             GameObject bruteGO = (GameObject)PrefabUtility.InstantiatePrefab(bruteFbx, root.transform);
             bruteGO.name = "character-1";
             bruteGO.transform.localPosition = Vector3.zero;
             bruteGO.transform.localRotation = Quaternion.identity;
             bruteGO.transform.localScale    = Vector3.one;
 
-            // 3. Setup Animator
             Animator anim = bruteGO.GetComponent<Animator>();
             if (anim == null) anim = bruteGO.GetComponentInChildren<Animator>(true);
             if (anim == null) anim = bruteGO.AddComponent<Animator>();
-
             anim.runtimeAnimatorController = bruteController;
             anim.applyRootMotion = false;
 
@@ -76,21 +72,17 @@ public static class BrutePlayerSetup
                 anim.avatar = fbxAnim.avatar;
             }
 
-            // 4. Find right hand bone
             Transform rightHand = FindRightHand(bruteGO.transform);
             Transform weaponParent = rightHand != null ? rightHand : bruteGO.transform;
 
-            // 5. Instantiate weapon models under weaponParent
             GameObject axe = LoadAndAttach(axePrefabPath, weaponParent, "wpn-axe-1", new Vector3(0.08f, 0.05f, 0.02f), Quaternion.Euler(0, 90, 90), Vector3.one * 0.8f);
             GameObject sword = LoadAndAttach(swordPrefabPath, weaponParent, "wpn-sword-1", new Vector3(0.08f, 0.05f, 0.02f), Quaternion.Euler(0, 90, 90), Vector3.one * 0.8f);
             GameObject hammer = LoadAndAttach(hammerPrefabPath, weaponParent, "wpn-hammer-1", new Vector3(0.08f, 0.05f, 0.02f), Quaternion.Euler(0, 90, 90), Vector3.one * 0.8f);
             GameObject trap = LoadAndAttach(trapPrefabPath, weaponParent, "trap", new Vector3(0.08f, 0.05f, 0.02f), Quaternion.Euler(0, 0, 0), Vector3.one * 0.3f);
             GameObject bomb = LoadAndAttach(bombPrefabPath, weaponParent, "wpn-bomb-1", new Vector3(0.08f, 0.05f, 0.02f), Quaternion.Euler(0, 0, 0), Vector3.one * 0.8f);
 
-            // 6. Setup Player component
             Player player = root.GetComponent<Player>();
             if (player == null) player = root.AddComponent<Player>();
-
             player.m_Animator = anim;
             player.m_MovementSpeed = 5f;
 
@@ -101,7 +93,6 @@ public static class BrutePlayerSetup
             cc.center = new Vector3(0f, 1f, 0f);
             player.m_PlayerController = cc;
 
-            // HitPoint
             Transform existingHp = root.transform.Find("HitPoint");
             if (existingHp != null)
             {
@@ -115,14 +106,12 @@ public static class BrutePlayerSetup
                 player.m_HitPoint = hpGo.transform;
             }
 
-            // Hit particle
             GameObject hpParticle = AssetDatabase.LoadAssetAtPath<GameObject>(hitParticlePath);
             if (hpParticle != null)
             {
                 player.m_HitParticlePrefab = hpParticle;
             }
 
-            // Weapon models array
             List<GameObject> weapons = new List<GameObject>();
             if (axe != null) weapons.Add(axe);
             if (sword != null) weapons.Add(sword);
@@ -131,18 +120,44 @@ public static class BrutePlayerSetup
             if (bomb != null) weapons.Add(bomb);
             player.m_WeaponModels = weapons.ToArray();
 
-            // Set axe active by default, others inactive
             if (axe != null) axe.SetActive(true);
             if (sword != null) sword.SetActive(false);
             if (hammer != null) hammer.SetActive(false);
             if (trap != null) trap.SetActive(false);
             if (bomb != null) bomb.SetActive(false);
-
-            Debug.Log("[BrutePlayerSetup] Brute character successfully configured as Player!");
         }
 
         AssetDatabase.SaveAssets();
         AssetDatabase.Refresh();
+        EnsureSampleSceneEnvironment();
+    }
+
+    private static void EnsureSampleSceneEnvironment()
+    {
+        const string scenePath = "Assets/Scenes/SampleScene.unity";
+        var scene = UnityEditor.SceneManagement.EditorSceneManager.OpenScene(scenePath, UnityEditor.SceneManagement.OpenSceneMode.Single);
+        GameObject root = GameObject.Find("EnvironmentGenerator");
+        if (root == null)
+        {
+            root = new GameObject("EnvironmentGenerator");
+            UnityEditor.SceneManagement.EditorSceneManager.MarkSceneDirty(scene);
+        }
+
+        var bootstrap = root.GetComponent<EnvironmentAutoBootstrap>();
+        if (bootstrap == null)
+        {
+            root.AddComponent<EnvironmentAutoBootstrap>();
+            UnityEditor.SceneManagement.EditorSceneManager.MarkSceneDirty(scene);
+        }
+
+        var generator = root.GetComponent<ProceduralEnvironmentGenerator>();
+        if (generator == null)
+        {
+            generator = root.AddComponent<ProceduralEnvironmentGenerator>();
+            UnityEditor.SceneManagement.EditorSceneManager.MarkSceneDirty(scene);
+        }
+
+        UnityEditor.SceneManagement.EditorSceneManager.SaveScene(scene);
     }
 
     private static Transform FindRightHand(Transform root)
